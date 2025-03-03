eb2ab3a6bca9d10d0f543a2e4e9e7d0be77c1a80
Sehara Gishan <sehara0927@gmail.com>
2025-03-03 22:45:45 +0530

    Adding Firebase and setup

--- finder/android/app/build.gradle.kts
+++ finder/android/app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.gms.google-services")
     
 }
 
@@ -45,4 +45,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:33.10.0"))
 }

70e9d5c40b3e21ae0f5194fb49367fc89e0e384f
Sehara Gishan <sehara0927@gmail.com>
2025-03-03 18:48:29 +0530

    Adding Firebase and setup

--- finder/lib/main.dart
+++ finder/lib/main.dart
@@ -6,13 +6,22 @@ import 'about_us.dart'; // About Us Page ✅
 import 'view_bookings.dart'; // Import the View Bookings Page
 import 'student_profile_page.dart'; // Import the Student Profile Page
 import 'lecturer_home.dart'; // Import Lecturer Home Page
+<<<<<<< HEAD
 import 'opening_page.dart';
+=======
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+>>>>>>> c949694 (Adding Firebase and setup)
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,6 +32,8 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
